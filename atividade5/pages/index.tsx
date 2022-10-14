2f075ff418dc97c5a429aa530050e7f6b7d82951
Ruana Rodrigues <ruana.rodrigues0@gmail.com>
2022-10-14 11:47:28 -0300

    Deploy

--- atividade5/pages/index.tsx
+++ atividade5/pages/index.tsx
@@ -1,7 +1,35 @@
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { CssBaseline, Typography } from '@mui/material'
+import { Box, Container } from '@mui/system'
 import React from 'react'
+import backgroundImage from "/components/images/atividade5.webp";
+import { red } from '@mui/material/colors';
 
+const theme = createTheme();
 export default function indexPages() {
   return (
-    <div>index</div>
+    
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box sx={{mt:8,
+        display:'flex',
+        flexDirection: 'column',
+        alignItems:'center'}}>
+          <Typography component="h1" variant="h5">
+          Executando React com Next.JS
+          </Typography>
+          <Typography component="h2">
+          Site criado em TypeScript publicado na Vercel.
+          </Typography>    
+          <Typography component="h3">
+          Grupo 5 - No rest for the interns
+          </Typography>    
+        </Box>
+      </Container> 
+    </ThemeProvider>
+
   )
 }
+
+
